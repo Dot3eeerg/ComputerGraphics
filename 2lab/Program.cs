ee8893db3a5125652faead6424ecde4ee619ca30
Ivan Danchenko <danchenko.ivan@inbox.ru>
2023-10-05 11:26:47 +0700

    TODO: SectionGenerator.cs

--- 2lab/Program.cs
+++ 2lab/Program.cs
@@ -3,6 +3,8 @@ using OpenTK.Mathematics;
 using OpenTK.Windowing.Common;
 using OpenTK.Windowing.Desktop;
 
+SectionGenerator sectionGenerator = new SectionGenerator("Input/Section", "Input/Replication");
+
 var nativeWindowSettings = new NativeWindowSettings()
 {
     Size = new Vector2i(1920, 1080),
